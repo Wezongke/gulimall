8f654850fb822acf35e1f4518b3e0f5615acfe2c
kaido <wzk12309@gmail.com>
2023-06-05 17:32:26 +0800

    load --

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,17 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+
     @Test
     void contextLoads() {
+
     }
 
 }
